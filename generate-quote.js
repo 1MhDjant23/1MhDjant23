9d0db7ac3774985f1657bc6ef77725bd1c826e96
MOHAMED AIT TAJANTE <96186148+1MhDjant23@users.noreply.github.com>
2025-11-10 17:16:40 +0100

    Duplicate quotes in generate-quote.js

--- generate-quote.js
+++ generate-quote.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 
 const quotes = [
-  "It's harder to read code than to write it. –  Mohamed Ait Tajante"
-  "Bad programmers worry about the code. Good programmers worry about the relationships between all the data in the system. –  Mohamed Ait Tajante"
-  "Make it work, make it right, make it fast. –  Mohamed Ait Tajante"
-  "Fools ignore complexity. Pragmatists suffer it. Some can avoid it. Geniuses remove it. –  Mohamed Ait Tajante"
-  "The function of good software is to make the complex appear to be simple."
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand. –  Mohamed Ait Tajante"
-  "Code never lies, comments sometimes do. –  Mohamed Ait Tajante"
-  "Give a man a program, frustrate him for a day. Teach a man to program, frustrate him for a lifetime. –  Mohamed Ait Tajante"
+  "It's harder to read code than to write it. –  Mohamed Ait Tajante",
+  "Bad programmers worry about the code. Good programmers worry about the relationships between all the data in the system. –  Mohamed Ait Tajante",
+  "Make it work, make it right, make it fast. –  Mohamed Ait Tajante",
+  "Fools ignore complexity. Pragmatists suffer it. Some can avoid it. Geniuses remove it. –  Mohamed Ait Tajante",
+  "The function of good software is to make the complex appear to be simple.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand. –  Mohamed Ait Tajante",
+  "Code never lies, comments sometimes do. –  Mohamed Ait Tajante",
+  "Give a man a program, frustrate him for a day. Teach a man to program, frustrate him for a lifetime. –  Mohamed Ait Tajante",
   "Code is like humor. When you have to explain it, it's bad. –  Mohamed Ait Tajante",
   "Talk is cheap. Show me the code. –  Mohamed Ait Tajante",
   "Programs must be written for people to read, and only incidentally for machines to execute. – Mohamed Ait Tajante",
